b29b1a5e9ccb4d4b829adb93f2c41fa1dd4137ad
Bruno <bl75044@gmail.com>
2025-06-02 20:55:17 -0300

    Commit

--- pãos/Program.cs
+++ pãos/Program.cs
@@ -4,16 +4,19 @@ Console.WriteLine("Digite o total de pão vendido");
 quantospães = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Digite o total de broas vendidas");
+
 quantasbroas = Convert.ToInt32(Console.ReadLine());
 
 Qpães = quantospães * 0.12m;
+
 Qbroas = quantasbroas * 1.50m;
 
 valorarrecadado = Qpães + Qbroas;
 
 valorguardado = valorarrecadado * 0.10m;
 
-Console.WriteLine("valorarrecadado : "+valorguardado);
-Console.WriteLine("O valorguardado na poupança (10%)R$" + valorguardado);
+Console.WriteLine("valorarrecadado : " + valorarrecadado);
+Console.WriteLine("O valorguardado na poupança (10%): R$" + valorguardado);
+
 
 
