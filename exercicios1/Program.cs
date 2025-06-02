670ee5bc84ed33dc4c7745fbc02617d6a73dd2c3
Aluno <Aluno@98XS314>
2025-06-02 19:37:06 -0300

    Add project files.

---
+++ exercicios1/Program.cs
@@ -0,0 +1,6 @@
+﻿int dimensões, área;
+Console.WriteLine("Diga as dimensões");
+dimensões= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Diga a área");
+área = Convert.ToInt32(Console.ReadLine());
---
+++ pãos/Program.cs
@@ -0,0 +1,19 @@
+﻿int quantospães, quantasbroas;
+decimal Qpães, Qbroas, valorarrecadado, valorguardado;
+Console.WriteLine("Digite o total de pão vendido");
+quantospães = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o total de broas vendidas");
+quantasbroas = Convert.ToInt32(Console.ReadLine());
+
+Qpães = quantospães * 0.12m;
+Qbroas = quantasbroas * 1.50m;
+
+valorarrecadado = Qpães + Qbroas;
+
+valorguardado = valorarrecadado * 0.10m;
+
+Console.WriteLine("valorarrecadado : "+valorguardado);
+Console.WriteLine("O valorguardado na poupança (10%)R$" + valorguardado);
+
+
