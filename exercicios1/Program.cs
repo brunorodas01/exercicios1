e8216b3a19e919aaea0b02508a06bad9e5703be0
Bruno <bl75044@gmail.com>
2025-06-04 19:51:34 -0300

    commit

--- exercicios1/Program.cs
+++ exercicios1/Program.cs
@@ -1,6 +1,1 @@
-﻿int dimensões, área;
-Console.WriteLine("Diga as dimensões");
-dimensões= Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Diga a área");
-área = Convert.ToInt32(Console.ReadLine());
+﻿
